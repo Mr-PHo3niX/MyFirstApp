26dc51110e42b6a5b6bbdf62c3475b0c1de19b87
MrPHo3niX <cr4zyas4d@gmail.com>
2022-02-08 10:31:52 +0300

    Add files via upload

---
+++ src/test.cpp
@@ -0,0 +1,26 @@
+#include <iostream> // input/output stream
+#include <math.h>
+#include <iomanip> // used to manipulate output
+#include <unistd.h>
+#include <term.h>
+#include <list>
+#include <vector>
+
+class youtubeChannel { // user-defined data type (to represent a real life object)
+	public:
+		std::string Name;
+		std::string OwnerName;
+		int SubscribersCount;
+		std::list<std::string> PublishedVideoTitles;
+};
+
+int main() {
+
+	youtubeChannel ytChannel;
+	ytChannel.Name = "CinnamonToastKen";
+	ytChannel.OwnerName = "Ken";
+	ytChannel.SubscribersCount = 5000000;
+	ytChannel.PublishedVideoTitles = {"90 days fiance Video 1", "90 days fiance Video 2", "90 days days fiance Video 3"};
+
+	return 0;
+}
